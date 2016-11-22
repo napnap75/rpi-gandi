a4538245ae6299ec376e9e3f6f9b28773d7d682f
napnap75 <thomas@nappez.com>
2016-11-22 12:14:13 +0100

    Update updatedns.sh

--- updatedns.sh
+++ updatedns.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-my_ip=$(curl -s https://api.ipify.org)
-echo "My public IP address is: $my_ip"
+# First check if the gandi cli is properly configured, otherwise configure it
+if ! (gandi config get api.key >> /dev/null && gandi config get api.host >> /dev/null); then
+  gandi config set api.host "https://rpc.gandi.net/xmlrpc/"
+  gandi config set api.key "$GANDI_API_KEY"
+fi
 
-echo "--- Help for setup"
-gandi help setup
-echo "--- Help for config"
-gandi help config
-echo "--- Help for record list"
-gandi help record list
-echo "--- Try record list"
-gandi record list $GANDI_DOMAIN
-echo "--- Help for record update"
-gandi help record update
+# Get my current IP
+my_ip=$(curl -s https://api.ipify.org)
+echo "My public IP address is: '$my_ip'"
+
+# Get my currently registered IP
+current_config=$(gandi record list -f json $GANDI_DOMAIN | jq '.[] | select(.name == "$GANDI_HOST")')
+current_ip=$(echo $current_config | jq -r '.value')
+echo "The current registered IP address is: '$current_ip'"
+
+# If they do not match, change it (and keep the TTL and TYPE)
+if [[ "$my_ip" != "$current_ip" ]]; then
+  host_string="$GANDI_HOST $(echo $current_config | jq -r '.ttl') IN TYPE $(echo $current_config | jq -r '.type')"
+  echo "Updating the record with: '$host_string $my_ip'"
+  gandi record update -r "$host_string $current_ip" --new-record "$host_string $my_ip"  $GANDI_DOMAIN 
+fi
